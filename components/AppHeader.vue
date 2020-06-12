cf344733b56b5ea6230e6a36fe504b6138fa0ff0
parthkamdar <parth.kamdar@gmail.com>
2020-06-12 14:32:44 +0530

    First commit

---
+++ components/AppHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <img class="logo" src="../assets/logo.jpg" />
+    <span class="login">Login</span>
+  </header>
+</template>
+<script>
+export default {}
+</script>
+<style scoped>
+header {
+  margin-bottom: 2rem;
+  padding: 2rem;
+  border-bottom: 1px solid #cccccc;
+}
+</style>
