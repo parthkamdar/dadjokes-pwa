06712109d3f2f5ab917d7142e526dc68b842f44f
parthkamdar <parth.kamdar@gmail.com>
2020-06-15 04:40:45 +0530

    Updates

--- sw.js
+++ sw.js
@@ -28,5 +28,5 @@ workbox.precaching.cleanupOutdatedCaches()
 // --------------------------------------------------
 
 // Register route handlers for runtimeCaching
-workbox.routing.registerRoute(new RegExp('/dadjokes-build/_nuxt/'), new workbox.strategies.CacheFirst ({}), 'GET')
-workbox.routing.registerRoute(new RegExp('/dadjokes-build/'), new workbox.strategies.NetworkFirst ({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/dadjokes-pwa/_nuxt/'), new workbox.strategies.CacheFirst ({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/dadjokes-pwa/'), new workbox.strategies.NetworkFirst ({}), 'GET')
