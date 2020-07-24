819a9bc97ebd6ca226421faf76b44c55b07a5d8f
Parth Kamdar <parth.kamdar@gmail.com>
2020-07-25 04:17:46 +0530

    config edited

--- nuxt.config.js
+++ nuxt.config.js
@@ -80,10 +80,5 @@ export default {
     publicPath: 'https://parthkamdar.github.io/dadjokes-pwa/',
     extractCSS: true,
     extend(config, ctx) {}
-  },
-  pwa: {
-    workbox: {
-      cleanupOutdatedCaches: true
-    }
   }
 }
